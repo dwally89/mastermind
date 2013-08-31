566f9c33e9c0004f02706407ca61564fb9a227ae
Daniel Wallis <dwally89@gmail.com>
2013-08-31 23:50:02 +0100

    Allow specifying options at the command line

--- src/com/waldm/mastermind/Mastermind.java
+++ src/com/waldm/mastermind/Mastermind.java
@@ -1,5 +1,7 @@
 package com.waldm.mastermind;
 
+import java.io.IOException;
+
 public class Mastermind {
     private final int codeLength;
     private final char[] alphabet;
@@ -7,21 +9,85 @@ public class Mastermind {
     private final Player guesser;
     private final CodeCreator codeCreator;
 
-    public Mastermind(int codeLength, char[] alphabet, int maximumNumberOfGuesses, Player guesser, CodeCreator codeCreator) {
-        this.codeLength = codeLength;
-        this.alphabet = alphabet;
-        this.maximumNumberOfGuesses = maximumNumberOfGuesses;
-        this.guesser = guesser;
-        this.codeCreator = codeCreator;
+    public Mastermind() {
+        System.out.println("Welcome to Mastermind!");
+        codeLength = askForCodeLength();
+        alphabet = askForAlphabet();
+        maximumNumberOfGuesses = askForMaximumNumberOfGuesses();
+        codeCreator = askForCodeCreator();
+        guesser = askForGuesser();
+
+        play();
     }
 
-    public static void main (String[] args) {
-        final int codeLength = 4;
-        final char[] alphabet = new char[]{'1', '2', '3', '4', '5', '6'};
-        Player guesser = new ComputerPlayer(codeLength, alphabet);
-        CodeCreator codeCreator = new HumanCodeCreator();
-        Mastermind mastermind = new Mastermind(codeLength, alphabet, 10, guesser, codeCreator);
-        mastermind.play();
+    private Player askForGuesser() {
+        System.out.println("Who will be guessing the code?");
+        System.out.println("Enter H for human or C for computer");
+        String guesserString;
+        try {
+            guesserString = Utils.readStringFromConsole();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read from console");
+        }
+
+        if (guesserString.equals("H")) {
+            return new HumanPlayer();
+        } else {
+            return new ComputerPlayer(codeLength, alphabet);
+        }
+    }
+
+    private CodeCreator askForCodeCreator() {
+        System.out.println("Who will be creating the code?");
+        System.out.println("Enter H for human or C for computer");
+        String codeCreatorString;
+        try {
+            codeCreatorString = Utils.readStringFromConsole();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read from console");
+        }
+
+        if (codeCreatorString.equals("H")) {
+            return new HumanCodeCreator();
+        } else {
+            return new ComputerCodeCreator(codeLength, alphabet);
+        }
+    }
+
+    private int askForMaximumNumberOfGuesses() {
+        System.out.println("What is the maximum number of guesses allowed? ");
+        try {
+            return Integer.parseInt(Utils.readStringFromConsole());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read from console");
+        }
+    }
+
+    private char[] askForAlphabet() {
+        System.out.println("What letters/numbers can the code contain?");
+        System.out.println("Enter the letters/numbers as a continuous string, with no spaces or commas");
+        String alphabetString = null;
+        try {
+            alphabetString = Utils.readStringFromConsole();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read from console");
+        }
+
+        char[] newAlphabet = new char[alphabetString.length()];
+        for (int i = 0; i < alphabetString.length(); i++) {
+            newAlphabet[i] = alphabetString.charAt(i);
+        }
+
+        return newAlphabet;
+    }
+
+    private int askForCodeLength() {
+        System.out.println("How long should the code be? ");
+        try {
+            return Integer.parseInt(Utils.readStringFromConsole());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read from console");
+        }
     }
 
     private void play() {
@@ -50,5 +116,9 @@ public class Mastermind {
         }
 
         guesser.informGameOver(codeCreator.getCode());
+    }
+
+    public static void main (String[] args) {
+        new Mastermind();
     }
 }
