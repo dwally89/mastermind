d7d0732e60ae8d6551a063b0994b0392d7de17fd
Daniel Wallis <dwally89@gmail.com>
2013-08-31 21:16:28 +0100

    Minor refactoring

--- src/com/waldm/mastermind/Mastermind.java
+++ src/com/waldm/mastermind/Mastermind.java
@@ -4,28 +4,30 @@ import java.io.*;
 import java.util.Random;
 
 public class Mastermind {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 8;
+    private static final int CODE_LENGTH = 4;
+    private static final int MAXIMUM_NUMBER_OF_GUESSES = 10;
+
+    private static class Result {
+        public final int locationCorrect;
+        public final int numberCorrect;
+
+        public Result(int locationCorrect, int numberCorrect) {
+            this.locationCorrect = locationCorrect;
+            this.numberCorrect = numberCorrect;
+        }
+    }
+
     public static void main (String[] args)
     {
-        final int MINIMUM_NUMBER = 1;
-        final int MAXIMUM_NUMBER = 8;
-        final int CODE_LENGTH = 4;
-        final int MAXIMUM_NUMBER_OF_GUESSES = 10;
-
-        final String code = generateCode(MINIMUM_NUMBER, MAXIMUM_NUMBER, CODE_LENGTH);
+        final String code = generateCode();
 
         int currentGuess = 1;
         while (currentGuess < MAXIMUM_NUMBER_OF_GUESSES) {
             System.out.println("Choose a " + CODE_LENGTH + " digit code, each digit between " + MINIMUM_NUMBER + "-" + MAXIMUM_NUMBER + ": ");
 
-            InputStreamReader isr = new InputStreamReader(System.in);
-            BufferedReader buffer = new BufferedReader(isr);
-            String guess = null;
-
-            try {
-                guess = buffer.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException("An input error has occurred");
-            }
+            String guess = getGuess();
 
             if (guess.length() != CODE_LENGTH) {
                 System.out.println("Make sure you enter " + CODE_LENGTH + " numbers");
@@ -36,7 +38,6 @@ public class Mastermind {
                 }
 
                 Result result = calculateResult(guess, code);
-
                 System.out.println(createFeedbackMessage(result));
 
                 currentGuess++;
@@ -44,6 +45,29 @@ public class Mastermind {
         }
 
         System.out.println("Game over, you lost, sorry :-(, the code was  " + code);
+    }
+
+    private static String getGuess() {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(isr);
+        String guess = null;
+
+        try {
+            guess = buffer.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("An input error has occurred");
+        }
+        return guess;
+    }
+
+    private static String generateCode() {
+        Random rand = new Random();
+        String code = "";
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            code += String.valueOf(rand.nextInt((MAXIMUM_NUMBER - MINIMUM_NUMBER) + 1) + MINIMUM_NUMBER);
+        }
+
+        return code;
     }
 
     private static Result calculateResult(String guess, String code) {
@@ -90,25 +114,5 @@ public class Mastermind {
 
         feedbackMessage += " in the wrong location";
         return feedbackMessage;
-    }
-
-    private static String generateCode(int min, int max, int length) {
-        Random rand = new Random();
-        String code = "";
-        for (int i = 0; i < length; i++) {
-            code += String.valueOf(rand.nextInt((max - min) + 1) + min);
-        }
-
-        return code;
-    }
-
-    private static class Result {
-        public final int locationCorrect;
-        public final int numberCorrect;
-
-        public Result(int locationCorrect, int numberCorrect) {
-            this.locationCorrect = locationCorrect;
-            this.numberCorrect = numberCorrect;
-        }
     }
 }
